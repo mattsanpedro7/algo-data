d55a54156583bb168d7eada0de4de88cd3386634
mattNat <mattsanpedro76@gmail.com>
2018-06-02 16:27:59 -0700

    finish reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/*****
+***
+**
+  turn number to string
+**
+***
+*/
+
+function reverseInt(n) {
+  return (Math.sign(n) * parseInt(n.toString().split('').reverse().join('')));
+}
 
 module.exports = reverseInt;

14f28e6c251beaf12e15bff2c9eed36e0e0c4aeb
mattNat <mattsanpedro76@gmail.com>
2018-06-02 14:49:10 -0700

    finish reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,14 +10,32 @@ function reverse(str) {
   // solution #1
   // return (str.split('').reverse().join(''))
 
-  // solution #2
-  let revStr = [];
-  for (let i = str.length; i--; i>str.length) {
-    revStr.push(str[i]);
-  }
-  return revStr.join('');
-}
+  // // solution #2
+  // let revStr = [];
+  // // should avoid for loops like this
+  // for (let i = str.length; i>str.length; i--) {
+  //   revStr.push(str[i]);
+  // }
+  // return revStr.join('');
 
-reverse('apple');
+  // // solution #3
+  // let reversed = '';
+  // for (let character of str) {
+  //   reversed = character + reversed;
+  //   // console.log(reversed);
+  // }
+  // return reversed;
+
+  // // solution #4
+  // // reduce takes 2 arguments: 1) arrow func, 2) starting init val for func
+  // return str.split('').reduce((reversed, character) => {
+  //   console.log(reversed, character);
+  //   return character + reversed;
+  // }, '');
+
+  // solution #5
+  debugger;
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;

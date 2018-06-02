2aca443df96ca07fe550564e2400b58d6797725b
mattNat <mattsanpedro76@gmail.com>
2018-06-02 15:54:37 -0700

    finish palidrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev);
+}
+
+// // solution #1
+// function palindrome(str) {
+//   const reversed = reverse(str);
+//   return str === reversed;
+// }
+
+// solution #2; not ideal but more to talk about
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+}
+
+console.log(palindrome('abbac'));
+
+// console.log(reverse('hello'));
+
 
 module.exports = palindrome;

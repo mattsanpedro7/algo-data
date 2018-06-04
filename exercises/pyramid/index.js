ebb2e90fc455dcf6c83e60c3684ba16cd8590856
mattNat <mattsanpedro76@gmail.com>
2018-06-04 16:40:10 -0700

    finish pyraamid iterative and recursive solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,72 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// // my solution
+// function pyramid(n) {
+//   // determine width of pyramid
+//   const width = 2*n - 1;
+
+//   // determine middle of pyramid
+//   const mid = Math.floor(width/2);
+
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < width; column++) {
+//       if (column > (mid + row) || column < (mid - row) ) {
+//         stair += ' ';
+//       } else {
+//         stair += '#';
+//       }
+//     }
+//     console.log(stair);
+    
+//   }
+// }
+
+// // solution #1: author iterative
+// function pyramid(n) {
+//   // calculate midpoint of row
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+// solution #2: author recursion
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1)
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
+
+pyramid(10)
 
 module.exports = pyramid;

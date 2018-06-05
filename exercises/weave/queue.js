550f5c411dc4cca0eaa84520cc05438cf0480f55
mattNat <mattsanpedro76@gmail.com>
2018-06-05 13:56:30 -0700

    weave finish peek function for queue.js

--- exercises/weave/queue.js
+++ exercises/weave/queue.js
@@ -16,6 +16,21 @@ class Queue {
   remove() {
     return this.data.pop();
   }
+
+  peek() {
+    return this.data.slice(-1);
+  }
 }
+
+// function main() {
+//   let Test = new Queue();
+//   Test.add(1);
+//   Test.add(2);
+//   Test.add(3);
+//   Test.add(4);
+
+//   Test.peek();
+// }
+// main();
 
 module.exports = Queue;

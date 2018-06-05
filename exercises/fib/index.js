d0668bb289e7de1c40d5b549ae47fade5e53642f
mattNat <mattsanpedro76@gmail.com>
2018-06-05 11:28:20 -0700

    finish fib iterative and recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// // solution #1: iterative
+// function fib(n) {
+//   let seq = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     seq.push(seq[i-1] + seq[i-2]);
+//   }
+
+//   return seq[n];
+// }
+
+// solution #2: recursive
+// exponential time solution
+let count = 0;
+function fib(n) {
+  count++;
+  // base case
+  if (n < 2) {
+    return n;
+    console.log('count', count);
+    
+  }
+
+  // n=2    1     +    0    = 1
+  // n=3    1     +    1    = 2
+  // n=4    2     +    1    = 3
+  // n=5    3     +    2    = 5
+  // n=6    5     +    3    = 8
+  return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(15));
 
 module.exports = fib;

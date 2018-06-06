0bf508db883098ed15e566bdf52a2c39ad6bfd77
mattNat <mattsanpedro76@gmail.com>
2018-06-06 16:02:25 -0700

    linkedlist - create node class and insertFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,34 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(record) {
+    console.log('HEAD:', this.head, 'DATA:', record);
+    this.head = new Node(record, this.head);
+  }
+
+  print() {
+    console.log(this.head);
+  }
+}
+
+function main() {
+  const LL = new LinkedList();
+  LL.insertFirst('Hi');
+  LL.insertFirst('Hello');  
+  LL.print();
+}
+main();
 
 module.exports = { Node, LinkedList };

ece45efa36281395f89c038b9a34cb24e97b24ee
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-06-06 17:40:43 -0700

    linkedlist - finish size and print

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -19,15 +19,45 @@ class LinkedList {
     this.head = new Node(record, this.head);
   }
 
+  insertLast(record) {
+    let tempNode = this.head;
+    while (tempNode) {
+      tempNode = tempNode.next;
+    }
+    console.log('LAST node', tempNode);
+    
+  }
+
+  size() {
+    let count = 0;
+    let tempNode = this.head;
+    while (tempNode) {
+      count++;
+      tempNode = tempNode.next;
+    }
+    return count;
+  }
+
   print() {
-    console.log(this.head);
+    let count = 0;
+    let tempNode = this.head;
+    while (tempNode) {
+      count++;
+      console.log('Position:', count, '/// Node:', tempNode.data);
+      tempNode = tempNode.next;
+    }
+    
   }
 }
 
 function main() {
   const LL = new LinkedList();
-  LL.insertFirst('Hi');
+  LL.insertFirst('Hey');
   LL.insertFirst('Hello');  
+  LL.insertFirst('How are you?');  
+
+  LL.insertLast('Fine thank you');
+
   LL.print();
 }
 main();

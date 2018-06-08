308cb13036268af0acd79b84fe116cae4617e216
mattNat <mattsanpedro76@gmail.com>
2018-06-08 09:53:18 -0700

    finish levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,49 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+const Node = require('./node');
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root, 's'];
+  let counters = [0];
+  // console.log(root.children);
+
+  // node = arr;
+  // console.log(node);
+  
+  
+  // take root and put 's'
+  // put children of root at end of array
+  // increase counter by 1
+  // throw out root
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
+
+function main() {
+  const root = new Node(0);
+  root.add(1);
+  root.add(2);
+  root.add(3);
+  root.children[0].add(4);
+  root.children[2].add(5);
+
+  levelWidth(root);
+
+  // console.log(JSON.stringify(root));
+  
+}
+main();
 
 module.exports = levelWidth;

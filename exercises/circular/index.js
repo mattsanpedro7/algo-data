372e1f85ef2c1ff74079d1a78df35e8bf4de5a30
mattNat <mattsanpedro76@gmail.com>
2018-06-07 19:06:29 -0700

    finish circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -11,7 +11,43 @@
 //   b.next = c;
 //   c.next = b;
 //   circular(l) // true
+const L = require('./linkedlist');
+const List = L.LinkedList;
+const Node = L.Node;
 
-function circular(list) {}
+function circular(list) {
+  // console.log(list);
+  
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    } else if (!fast.next || !fast.next.next) {
+      // console.log('next DNE');
+      return false;
+    }
+  } 
+
+  return false;
+}
+
+function main() {
+  const l = new List();
+  const a = new Node('a');
+  const b = new Node('b');
+  const c = new Node('c');
+  l.head = a;
+  a.next = b;
+  b.next = c;
+  c.next = b;
+
+  console.log(circular(l));
+}
+main();
 
 module.exports = circular;

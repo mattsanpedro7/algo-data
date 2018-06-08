f79473505667640998e17611a81cd352c6f20e47
mattNat <mattsanpedro76@gmail.com>
2018-06-08 13:39:33 -0700

    finish validate bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -4,7 +4,41 @@
 // less than the parent node's value, and that
 // every node's right hand child is greater than
 // the parent
+const Node = require('./node');
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // console.log(node.data, node.left.data, node.right.data);
+
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
+
+function main() {
+  const n = new Node(10);
+  n.insert(5);
+  n.insert(15);
+  n.insert(0);
+  n.insert(20);
+  
+  // console.log(n);
+  
+  validate(n);
+}
+main();
 
 module.exports = validate;

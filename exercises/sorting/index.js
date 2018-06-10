2e20935ce0402b5923c33a12f42b97aa27d08149
mattNat <mattsanpedro76@gmail.com>
2018-06-09 17:25:15 -0700

    finish bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,10 +3,23 @@
 
 function bubbleSort(arr) {
   // implement bubblesort
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      // swap
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+      // console.log(arr);
+    }
+  }
 
   // return sorted array
+  return arr;
 }
 
+// "prove me wrong" algorithm
 function selectionSort(arr) {
 
 }
@@ -18,5 +31,19 @@ function mergeSort(arr) {
 function merge(left, right) {
 
 }
+
+function mainBubble() {
+  const arr = [2, 9, 94, 8, 4, 5, 10, 26, 13, 99, 52];
+
+  bubbleSort(arr);
+}
+// mainBubble();
+
+function mainSelectionSort() {
+  const arr = [2, 9, 94, 8, 4, 5, 10, 26, 13, 99, 52];
+
+  
+}
+mainSelectionSort();
 
 module.exports = { bubbleSort, selectionSort, mergeSort };

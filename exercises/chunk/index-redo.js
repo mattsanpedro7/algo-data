76e3e36a04b16d9105de5a3a7b9348c8a1335fef
mattNat <mattsanpedro76@gmail.com>
2018-06-11 11:48:53 -0700

    redo chuck

---
+++ exercises/chunk/index-redo.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+// solution #2
+function chunk(array, size) {
+  const result = [];
+  let index = 0;
+  while (index < array.length) {
+    result.push(array.slice(index, index + size));
+    index = index + size;
+  }
+
+  console.log(result);
+  
+  return result;
+}
+
+chunk([1, 2, 3, 4, 5], 2);
+
+module.exports = chunk;

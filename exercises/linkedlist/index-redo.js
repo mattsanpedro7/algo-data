8d785faa7674eafd812fe942b55b133b96b073bf
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-06-11 18:15:28 -0700

    redo linkedlist - insertFirst, insertLast, getFirst, getLast, print

---
+++ exercises/linkedlist/index-redo.js
@@ -0,0 +1,88 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(record) {
+    this.head = new Node(record, this.head);
+  }
+
+  insertLast(record) {
+    let tempNode = this.head;
+    while (tempNode.next) {
+      tempNode = tempNode.next;
+    }
+    // console.log(tempNode);
+    tempNode.next = new Node(record, null);
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let tempNode = this.head;
+    while (tempNode.next) {
+      tempNode = tempNode.next;
+    }
+    // console.log(tempNode);
+    return tempNode;
+  }
+
+  print() {
+    let tempNode = this.head;
+    let count = 0;
+    console.log('PRINTING');
+    
+    while (tempNode) {
+      count++;
+      console.log('\tNode:', count, '/// Data:', tempNode.data);
+      tempNode = tempNode.next;
+    }
+  }
+}
+
+function main() {
+  const LL = new LinkedList();
+  // LL.insertFirst('a');
+  LL.insertFirst('Hello');  
+  LL.insertFirst('How are you?');  
+
+  LL.insertLast('Fine thank you');
+  LL.insertLast('b');
+  
+  console.log('GetFirst:', LL.getFirst());
+  console.log('GetLast:', LL.getLast());  
+  // LL.removeFirst();
+  
+  // LL.print();  
+  // LL.removeLast();
+  // LL.print();
+  // console.log(JSON.stringify(LL));
+  
+  // LL.clear();
+
+  // LL.getAt(2);
+
+  // console.log(LL.size());
+  
+  // LL.removeAt(1);
+
+  // LL.print();
+  // LL.insertAt('Bye Bye Bye', 30)
+  LL.print();
+}
+main();
+
+module.exports = { Node, LinkedList };
